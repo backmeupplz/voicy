f8f705017d228c0198f7bcb1af3afa5013e3dcdd
backmeupplz <backmeupplz@gmail.com>
2018-07-30 22:12:15 -0700

    add filter message

--- helpers/filter.js
+++ helpers/filter.js
@@ -4,6 +4,10 @@
  */
 function checkDate(ctx) {
   const message = ctx.update.message || ctx.update.channelPost
+  if (!message) {
+    console.info('Not processing because no message found', JSON.stringify(ctx.update, undefined, 2))
+    return false
+  }
   const isMsgNew = (Date.now() / 1000) - message.date < 5 * 60
   if (!isMsgNew) {
     console.info('Not processing message', message.date, JSON.stringify(message, undefined, 2))
