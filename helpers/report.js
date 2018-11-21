0261893358c3823c7c9c929ed9372768f0748595
backmeupplz <backmeupplz@gmail.com>
2018-11-21 00:26:35 -0800

    add valid error

--- helpers/report.js
+++ helpers/report.js
@@ -4,6 +4,7 @@ module.exports = function(bot, err, prefix) {
       'message to edit not found',
       'does not contain any stream',
       'Invalid data found',
+      'reply message not found',
     ]
     for (const item of bypassList) {
       if (err.message && err.message.indexOf(item) > -1) {
