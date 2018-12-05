c51ecc8004b6b2d6e5e9e7420496acca5e23cc2a
backmeupplz <backmeupplz@gmail.com>
2018-12-04 23:17:57 -0800

    add valid error

--- helpers/report.js
+++ helpers/report.js
@@ -19,6 +19,7 @@ module.exports = function(bot, err, prefix) {
       'wrong file id',
       'End of file',
       'does not have storage.buckets.create access to project',
+      'bot is not a member',
     ]
     for (const item of bypassList) {
       if (err.message && err.message.indexOf(item) > -1) {
