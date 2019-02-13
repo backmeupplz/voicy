106285adaa2117e34e243d1858430a3cb25f5d94
backmeupplz <backmeupplz@gmail.com>
2019-02-12 21:12:57 -0800

    filter nonaudio

--- helpers/handler.js
+++ helpers/handler.js
@@ -12,7 +12,6 @@ function setupAudioHandler(bot) {
   bot.on(['voice', 'video_note'], ctx => {
     // Check if less than 5 minutes ago
     if (!checkDate(ctx)) return
-
     // Handle voice
     handleMessage(ctx)
   })
@@ -20,13 +19,31 @@ function setupAudioHandler(bot) {
   bot.on(['audio', 'document'], async ctx => {
     // Check if less than 5 minutes ago
     if (!checkDate(ctx)) return
-
     // Check if files banned
     const chat = await findChat(ctx.chat.id)
     if (chat.filesBanned) return
+    // Check if correct format
+    if (!isCorrectDocument(ctx)) {
+      return
+    }
     // Handle voice
     handleMessage(ctx)
   })
+}
+
+function isCorrectDocument(ctx) {
+  const message = ctx.message || ctx.update.channel_post
+  if (!message.document) {
+    return true
+  }
+  const mime = message.document.mime_type
+  const allowedMimeTypes = ['audio', 'octet-stream']
+  for (const allowedType of allowedMimeTypes) {
+    if (mime.indexOf(allowedType) > -1) {
+      return true
+    }
+  }
+  return false
 }
 
 // Exports
