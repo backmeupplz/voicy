2128538829642ec86d989b28901be0192a2a66a4
Nikita Kolmogorov <backmeupplz@gmail.com>
2019-02-27 11:11:30 -0800

    Update timeReceived.js

--- middlewares/timeReceived.js
+++ middlewares/timeReceived.js
@@ -1,7 +1,7 @@
 module.exports = function setupAddingTimeReceived(bot) {
   bot.use((ctx, next) => {
     if (ctx.message && ctx.message.date) {
-      console.log(`Received message "${ctx.message.text}" ${ctx.message.date}`)
+      console.log(`Received message ${(Date.now() / 1000) - ctx.message.date}s ago`)
     }
     ctx.timeReceived = new Date()
     next()
