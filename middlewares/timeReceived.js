d8a09eeee0ab718b57b0071a0bed686e8ec7efe5
Nikita Kolmogorov <backmeupplz@gmail.com>
2019-03-01 09:18:35 -0800

    Update timeReceived.js

--- middlewares/timeReceived.js
+++ middlewares/timeReceived.js
@@ -5,7 +5,7 @@ module.exports = function setupAddingTimeReceived(bot) {
     ctx.timeReceived = new Date()
     next()
     
-    if (ctx.update && ctx.update.date) {
+    if (ctx.update.message && ctx.update.message.date) {
       fs.appendFile(`${__dirname}/../updates.log`, `${Math.floor(Date.now() / 1000)} — ${Math.floor(Date.now() / 1000) - ctx.update.date}s`);
     }
   })
