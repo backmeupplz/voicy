8462eafd4d91dcacbed37aef7e0c8e9cdd681500
Nikita Kolmogorov <backmeupplz@gmail.com>
2019-03-05 15:59:12 -0800

    Update speechAPI.js

--- helpers/speechAPI.js
+++ helpers/speechAPI.js
@@ -90,7 +90,7 @@ async function wit(token, filePath, duration, iLanguage) {
     for (const path of pathsToRecognize) {
       promises.push(
         new Promise(async (res, rej) => {
-          let triesCount = 5
+          let triesCount = 1
           let error
           while (triesCount > 0) {
             try {
