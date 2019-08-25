d9ad4112047022d1916c915dd3d3b5183b08afd7
backmeupplz <backmeupplz@gmail.com>
2019-08-24 18:27:38 -0700

    add url

--- commands/url.js
+++ commands/url.js
@@ -1,7 +1,6 @@
 // Dependencies
 const logAnswerTime = require('../helpers/logAnswerTime')
 const checkAdminLock = require('../middlewares/adminLock')
-const { reportUsage } = require('../helpers/report')
 
 function setupUrl(bot) {
   bot.command('url', checkAdminLock, ctx => {
@@ -10,9 +9,8 @@ function setupUrl(bot) {
 }
 
 async function handle(ctx) {
-  await ctx.replyWithMarkdown(ctx.i18n.t('oops'))
+  await ctx.replyWithMarkdown(ctx.i18n.t('url'))
   logAnswerTime(ctx, '/url')
-  reportUsage(ctx, '/url')
 }
 
 // Exports
