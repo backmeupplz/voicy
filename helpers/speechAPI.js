2dded2fb47da1130f2c7b33d64367f58cbc5ab13
backmeupplz <backmeupplz@gmail.com>
2019-09-20 11:20:43 -0700

    turn off headers

--- helpers/speechAPI.js
+++ helpers/speechAPI.js
@@ -180,7 +180,7 @@ async function ashmanov(path, duration) {
       method: 'post',
       url: 'https://asr.ashmanov.org/asr/',
       data: formData,
-      headers,
+      // headers,
     })
     return [[`0-${parseInt(duration, 10)}`, JSON.stringify(response)]]
   } catch (err) {
