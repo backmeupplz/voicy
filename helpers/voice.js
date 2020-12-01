7c2819aed42bba983a5f2be79379e5a47ffb034d
backmeupplz <backmeupplz@gmail.com>
2020-12-01 07:59:20 -0800

    rollback promo

--- helpers/voice.js
+++ helpers/voice.js
@@ -29,7 +29,7 @@ const promoExceptions = [
 
 const promoTexts = {
   en: 'Powered by [Todorant](https://todorant.com/?ref=voicy)',
-  ru: 'При поддержке [Золота Бородача](https://t.me/golden_borodutch)',
+  ru: 'Powered by [Todorant](https://todorant.com/?ref=voicy)',
 }
 
 /**
