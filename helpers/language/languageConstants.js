564a46894e81a0c7b5600c0a5be6325605e35c3b
backmeupplz <backmeupplz@gmail.com>
2021-05-22 10:57:55 -0700

    add logging

--- helpers/language/languageConstants.js
+++ helpers/language/languageConstants.js
@@ -8,6 +8,9 @@ function localeForChat(chat) {
     engineObject.languages.find(
       (l) => l.code === engineObject.defaultLanguageCode
     )
+  if (chat.id === 76104711) {
+    console.log(engineObject, language, languageObject)
+  }
   return languageObject.i18nCode
 }
 
