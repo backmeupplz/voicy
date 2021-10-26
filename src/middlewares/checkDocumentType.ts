2fd805fb49525d81d135adb424de3ae71832179e
backmeupplz <backmeupplz@gmail.com>
2021-10-26 16:08:27 -0700

    fix document mime type check

--- src/middlewares/checkDocumentType.ts
+++ src/middlewares/checkDocumentType.ts
@@ -2,6 +2,9 @@ import { NextFunction } from 'grammy'
 import Context from '@/models/Context'
 
 export default function checkDocumentType(ctx: Context, next: NextFunction) {
+  if (!ctx.msg) {
+    return
+  }
   const mime = ctx.msg.document.mime_type
   const allowedMimeTypes = ['audio', 'octet-stream']
   for (const allowedType of allowedMimeTypes) {
