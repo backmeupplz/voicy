054ed6ea837fa35c2ae7edc8fe5a93882812bd2d
backmeupplz <backmeupplz@gmail.com>
2021-10-26 16:07:06 -0700

    fix promo exceptions

--- src/commands/handleAddPromoException.ts
+++ src/commands/handleAddPromoException.ts
@@ -2,10 +2,12 @@ import { addPromoException } from '@/models/PromoException'
 import Context from '@/models/Context'
 
 export default async function handleAddPromoException(ctx: Context) {
-  const id = +ctx.message.text.split(' ')[1]
-  if (!id) {
+  const ids = ctx.message.text.split(' ')[1]
+  if (!ids) {
     return ctx.reply('😱')
   }
-  await addPromoException(id)
+  for (const id of ids.split(',')) {
+    await addPromoException(+id)
+  }
   return ctx.reply('👍')
 }
