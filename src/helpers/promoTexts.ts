eecf5ace6f4560e30cefb75891edad0727350202
backmeupplz <backmeupplz@gmail.com>
2021-11-12 09:41:07 -0800

    change promo back

--- src/helpers/promoTexts.ts
+++ src/helpers/promoTexts.ts
@@ -1,10 +1,6 @@
 const promoTexts = {
-  // ru: () => 'При поддержке [Бородач Инвест](https://invest.borodutch.com)',
-  // en: () => 'Powered by [Borodutch Invest](https://invest.borodutch.com)',
-  ru: () =>
-    '[Спасите разработчиков Телеграм-ботов](https://t.me/golden_borodutch/5777)',
-  en: () =>
-    '[Save Telegram bot developers](https://t.me/borodutch_support/172)',
+  ru: () => 'При поддержке [Бородач Инвест](https://invest.borodutch.com)',
+  en: () => 'Powered by [Borodutch Invest](https://invest.borodutch.com)',
 }
 
 export default promoTexts
