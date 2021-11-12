d27d1887c433ac18787cbbec672ede147d1ef849
backmeupplz <backmeupplz@gmail.com>
2021-11-12 09:33:45 -0800

    change promo message

--- src/helpers/promoTexts.ts
+++ src/helpers/promoTexts.ts
@@ -1,6 +1,10 @@
 const promoTexts = {
-  ru: () => 'При поддержке [Бородач Инвест](https://invest.borodutch.com)',
-  en: () => 'Powered by [Borodutch Invest](https://invest.borodutch.com)',
+  // ru: () => 'При поддержке [Бородач Инвест](https://invest.borodutch.com)',
+  // en: () => 'Powered by [Borodutch Invest](https://invest.borodutch.com)',
+  ru: () =>
+    '[Спасите разработчиков Телеграм-ботов](https://t.me/borodutch_support/172)',
+  en: () =>
+    '[Save Telegram bot developers](https://t.me/borodutch_support/172)',
 }
 
 export default promoTexts
