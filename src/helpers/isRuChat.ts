909370ad644aea29fcb28254dfa66ad41664db84
Nikita Kolmogorov <backmeupplz@gmail.com>
2021-12-13 08:54:16 -0800

    add russian recognizer in promo

--- src/helpers/isRuChat.ts
+++ src/helpers/isRuChat.ts
@@ -5,6 +5,9 @@ export default function isRuChat(chat: Chat) {
   return (
     chat.engine === Engine.ashmanov ||
     (chat.engine === Engine.wit && chat.languages[Engine.wit] === 'Russian') ||
-    (chat.engine === Engine.google && chat.languages[Engine.google] === 'ru-RU')
+    (chat.engine === Engine.google &&
+      chat.languages[Engine.google] === 'ru-RU') ||
+    (chat.engine === Engine.platinumfund &&
+      chat.languages[Engine.platinumfund] === 'Russian')
   )
 }
