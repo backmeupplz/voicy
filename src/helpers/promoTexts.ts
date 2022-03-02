1bd6f1edb5cf7821a7377ebb77a804c52e7a87f6
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-03-02 09:06:48 -0800

    fix spelling

--- src/helpers/promoTexts.ts
+++ src/helpers/promoTexts.ts
@@ -1,6 +1,6 @@
 const promoTexts = {
   ru: () =>
-    'Путин и его свита [убивают мирное население](https://t.me/verkhovnaradaofukraine) в войне в Украине #stopputin',
+    'Путин и его свита [убивают мирное население](https://t.me/verkhovnaradaofukraine) на войне в Украине #stopputin',
   en: () =>
     'Putin and his cronies [kill civilians](https://t.me/verkhovnaradaofukraine) in the war in Ukraine #stopputin',
 }
