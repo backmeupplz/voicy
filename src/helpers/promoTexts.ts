1da565a80ab8f2681fddbcf443df60e6a7a15fa5
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-03-01 19:12:14 -0800

    change text

--- src/helpers/promoTexts.ts
+++ src/helpers/promoTexts.ts
@@ -1,6 +1,8 @@
 const promoTexts = {
-  ru: () => 'При поддержке [Бородач Инвест](https://invest.borodutch.com)',
-  en: () => 'Powered by [Borodutch Invest](https://invest.borodutch.com)',
+  ru: () =>
+    'Путин и его свита [убивают мирное население](https://t.me/verkhovnaradaofukraine) в войне на Украине #stopputin',
+  en: () =>
+    'Putin and his cronies [kill civilians](https://t.me/verkhovnaradaofukraine) in the war in Ukraine #stopputin',
 }
 
 export default promoTexts
