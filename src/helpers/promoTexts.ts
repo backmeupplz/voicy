0d90cb8cb64f2e24db91e413c491176ca16e8413
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-06-19 13:20:45 -0700

    change promo

--- src/helpers/promoTexts.ts
+++ src/helpers/promoTexts.ts
@@ -1,8 +1,12 @@
 const promoTexts = {
+  // ru: () =>
+  //   'Путин и его свита [убивают мирное население](https://t.me/verkhovnaradaofukraine) на войне в Украине #stopputin',
+  // en: () =>
+  //   'Putin and his cronies [kill civilians](https://t.me/verkhovnaradaofukraine) in the war in Ukraine #stopputin',
   ru: () =>
-    'Путин и его свита [убивают мирное население](https://t.me/verkhovnaradaofukraine) на войне в Украине #stopputin',
+    'Телеграм [забрал функцию](https://t.me/borodutch_support/188) из Войси и даже не сказал спасибо, [поддержите создателя Войси](https://t.me/borodutch_support)',
   en: () =>
-    'Putin and his cronies [kill civilians](https://t.me/verkhovnaradaofukraine) in the war in Ukraine #stopputin',
+    'Telegram [took a function](https://t.me/borodutch_support/188) from Voicy and didn\'t even say thanks, [support the creator of Voicy](https://t.me/borodutch_support)',
 }
 
 export default promoTexts
