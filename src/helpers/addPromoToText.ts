0f62f5efd69213d30eb02928a602071e21c35ee0
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-08-23 11:49:53 -0700

    turn voicy back on

--- src/helpers/addPromoToText.ts
+++ src/helpers/addPromoToText.ts
@@ -1,13 +1,10 @@
-// import { promoExceptions } from '@/models/PromoException'
+import { promoExceptions } from '@/models/PromoException'
 import Context from '@/models/Context'
 import isRuChat from '@/helpers/isRuChat'
-// import promoTexts from '@/helpers/promoTexts'
+import promoTexts from '@/helpers/promoTexts'
 
 export default function addPromoToText(ctx: Context, text: string) {
-  // return promoExceptions.includes(+ctx.dbchat.id)
-  //   ? text
-  //   : `${text}\n${isRuChat(ctx.dbchat) ? promoTexts.ru() : promoTexts.en()}`
-  return isRuChat(ctx.dbchat)
-    ? '_Друзья, простите, но я больше не могу терпеть игнорирование со стороны команды Телеграма. Пожалуйста, напишите, насколько сильно вам нужен Войси в официальном чате Телеграма — @durovschat. 13 миллионов людей пользуется Войси каждый день, но надо мной — и другими сторонними разработчиками — Телеграм просто смеется.\n\nМы теряем Войси! Спросите у команды Телеграма в @durovschat, почему Войси не работает и кто получил призы за ботов (telegram.org/blog/botprize). Спасибо вам большое, что вы со мной! Всегда ваш, @borodutch_ ❤️'
-    : "_Friends, I'm sorry, but I can no longer withstand the Telegram team's public disregard of Voicy existence. Please tell the Telegram team how helpful Voicy is to you in the official Telegram chat — @durovschat. 13 million people use Voicy daily, but Telegram laughs at other bot developers and me.\n\nWe are losing Voicy! Ask the Telegram team at @durovschat why doesn't Voicy work and who got the bot prize (telegram.org/blog/botprize). Thank you a lot for your support! Always yours, @borodutch_ ❤️"
+  return promoExceptions.includes(+ctx.dbchat.id)
+    ? text
+    : `${text}\n${isRuChat(ctx.dbchat) ? promoTexts.ru() : promoTexts.en()}`
 }
