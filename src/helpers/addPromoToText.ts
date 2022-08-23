10a18b3119bfbcf06b6d2253c0b240a1e1209ffc
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-08-22 19:20:36 -0700

    change the text

--- src/helpers/addPromoToText.ts
+++ src/helpers/addPromoToText.ts
@@ -1,10 +1,13 @@
-import { promoExceptions } from '@/models/PromoException'
+// import { promoExceptions } from '@/models/PromoException'
 import Context from '@/models/Context'
 import isRuChat from '@/helpers/isRuChat'
-import promoTexts from '@/helpers/promoTexts'
+// import promoTexts from '@/helpers/promoTexts'
 
 export default function addPromoToText(ctx: Context, text: string) {
-  return promoExceptions.includes(+ctx.dbchat.id)
-    ? text
-    : `${text}\n${isRuChat(ctx.dbchat) ? promoTexts.ru() : promoTexts.en()}`
+  // return promoExceptions.includes(+ctx.dbchat.id)
+  //   ? text
+  //   : `${text}\n${isRuChat(ctx.dbchat) ? promoTexts.ru() : promoTexts.en()}`
+  return isRuChat(ctx.dbchat)
+    ? 'Друзья, простите, но я больше не могу терпеть игнорирование со стороны команды Телеграма. Пожалуйста, напишите, насколько сильно вам нужен Войси в официальном чате Телеграма — @tdlibchat. 13 миллионов людей пользуется Войси каждый день, но надо мной — и другими сторонними разработчиками — Телеграм просто смеется. Спасибо вам большое, что вы со мной! Всегда ваш, @borodutch_support ❤️'
+    : "Friends, I'm sorry, but I can no longer withstand the Telegram team's public disregard of Voicy existence. Please note how useful Voicy is to you in the official Telegram chat — @tdlibchat. 13 million people use Voicy daily, but Telegram laughs at other bot developers and me. Thank you a lot for your support! Always yours, @borodutch_support ❤️"
 }
