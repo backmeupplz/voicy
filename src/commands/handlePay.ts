0daa8aecee4d32099da8052b5171e5f58c45fb65
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-10-30 10:16:49 -0700

    add logging

--- src/commands/handlePay.ts
+++ src/commands/handlePay.ts
@@ -9,35 +9,39 @@ export default async function handlePay(ctx: Context) {
       disable_web_page_preview: true,
     })
   } else {
-    await ctx.api.sendChatAction(ctx.chat.id, 'typing')
-    const session = await stripe.checkout.sessions.create({
-      payment_method_types: ['card'],
-      line_items: [
-        {
-          price: 'price_1LyeHbKXsMRGkVL4i2xZnaZk',
-          quantity: 1,
-        },
-      ],
-      success_url: 'https://t.me/voicybot',
-      cancel_url: 'https://t.me/voicybot',
-      client_reference_id: `${ctx.dbchat.id}`,
-      mode: 'payment',
-      allow_promotion_codes: true,
-    })
-    await ctx.reply(ctx.i18n.t('pay'), {
-      parse_mode: 'Markdown',
-      disable_web_page_preview: true,
-      reply_markup: {
-        inline_keyboard: [
-          [
-            {
-              text: ctx.i18n.t('pay_button'),
-              url: session.url,
-            },
-          ],
+    try {
+      await ctx.api.sendChatAction(ctx.chat.id, 'typing')
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items: [
+          {
+            price: 'price_1LyeHbKXsMRGkVL4i2xZnaZk',
+            quantity: 1,
+          },
         ],
-      },
-    })
+        success_url: 'https://t.me/voicybot',
+        cancel_url: 'https://t.me/voicybot',
+        client_reference_id: `${ctx.dbchat.id}`,
+        mode: 'payment',
+        allow_promotion_codes: true,
+      })
+      await ctx.reply(ctx.i18n.t('pay'), {
+        parse_mode: 'Markdown',
+        disable_web_page_preview: true,
+        reply_markup: {
+          inline_keyboard: [
+            [
+              {
+                text: ctx.i18n.t('pay_button'),
+                url: session.url,
+              },
+            ],
+          ],
+        },
+      })
+    } catch (error) {
+      console.log('error sending checkout', error)
+    }
   }
   logAnswerTime(ctx, '/pay')
 }
