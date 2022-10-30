2bb20e80e0e882b12b8ce3433e41f73fbe46a8e9
Nikita Kolmogorov <backmeupplz@gmail.com>
2022-10-30 10:25:51 -0700

    remove promo

--- src/helpers/addPromoToText.ts
+++ src/helpers/addPromoToText.ts
@@ -1,10 +1,11 @@
-import { promoExceptions } from '@/models/PromoException'
+// import { promoExceptions } from '@/models/PromoException'
 import Context from '@/models/Context'
-import isRuChat from '@/helpers/isRuChat'
-import promoTexts from '@/helpers/promoTexts'
+// import isRuChat from '@/helpers/isRuChat'
+// import promoTexts from '@/helpers/promoTexts'
 
 export default function addPromoToText(ctx: Context, text: string) {
-  return promoExceptions.includes(+ctx.dbchat.id)
-    ? text
-    : `${text}\n${isRuChat(ctx.dbchat) ? promoTexts.ru() : promoTexts.en()}`
+  // return promoExceptions.includes(+ctx.dbchat.id)
+  //   ? text
+  //   : `${text}\n${isRuChat(ctx.dbchat) ? promoTexts.ru() : promoTexts.en()}`
+  return text
 }
