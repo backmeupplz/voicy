96caf09c45e4d54a9b3978976decd5ae82f4e883
backmeupplz <backmeupplz@gmail.com>
2026-05-05 11:33:46 -0700

    Upgrade dependency paths for security audit

--- src/helpers/stripe.ts
+++ src/helpers/stripe.ts
@@ -1,5 +1,6 @@
 import Stripe from 'stripe'
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2022-08-01',
+  // Keep requests on the account API version this bot already used.
+  apiVersion: '2022-08-01' as Stripe.LatestApiVersion,
 })
